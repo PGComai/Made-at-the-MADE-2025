8059289eaff4d1cc3b3fb198d1582d8d16259bae
Ava Pek <code@exodrifter.space>
2025-12-28 15:54:32 -0800

    Always use rectangle shape for checkpoint

--- scenes/checkpoint/checkpoint.gd
+++ scenes/checkpoint/checkpoint.gd
@@ -5,18 +5,22 @@ extends Area2D
 signal car_entered(car: Node2D)
 signal car_exited(car: Node2D)
 
-@export var shape: Shape2D:
+@export var width: float = 0:
 	set(value):
-		if shape != value:
-			shape = value
+		if width != value:
+			width = value
 			if is_node_ready():
+				var shape := RectangleShape2D.new()
+				shape.size = Vector2(width, 20)
 				collision_shape.shape = shape
-				update_configuration_warnings()
 
 @onready var collision_shape: CollisionShape2D = %CollisionShape2D
 
 func _ready() -> void:
+	var shape := RectangleShape2D.new()
+	shape.size = Vector2(width, 20)
 	collision_shape.shape = shape
+
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
 
@@ -27,8 +31,3 @@ func _on_body_entered(body: Node2D) -> void:
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("car"):
 		car_exited.emit(body)
-
-func _get_configuration_warnings() -> PackedStringArray:
-	if shape == null:
-		return ["A shape must be provided for Checkpoint to function."]
-	return []
