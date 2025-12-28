1c9bbb8aba64645e71a69164493878c1f7e8f4e4
Ava Pek <code@exodrifter.space>
2025-12-28 12:14:19 -0800

    Add test racetrack with checkpoint recorder

---
+++ scenes/checkpoint_recorder.gd
@@ -0,0 +1,42 @@
+## Keeps track of the checkpoints that have been entered by each body.
+class_name CheckpointRecorder
+extends Node
+
+## Emitted when a body has visited all checkpoints
+signal seen_all_checkpoints(body: Node2D)
+
+## A list of all checkpoints in the level.
+var all_checkpoints: Array[Checkpoint] = []
+
+## Each body and the checkpoints that it has seen.
+var seen: Dictionary = {}
+
+func _ready() -> void:
+	all_checkpoints.clear()
+	reset()
+
+	for node in get_tree().get_nodes_in_group("checkpoint"):
+		if node is not Checkpoint:
+			print(node.get_path(), " is in the checkpoint group but is not a checkpoint!")
+			continue
+
+		var checkpoint: Checkpoint = node
+		var callable := record_checkpoint.bind(checkpoint)
+		if not checkpoint.car_entered.is_connected(callable):
+			checkpoint.car_entered.connect(callable)
+
+		all_checkpoints.push_back(checkpoint)
+
+func reset() -> void:
+	seen.clear()
+
+func record_checkpoint(body: Node2D, checkpoint: Checkpoint) -> void:
+	if not seen.has(body):
+		seen[body] = []
+	seen[body].push_back(checkpoint)
+
+	if seen[body] == all_checkpoints:
+		seen_all_checkpoints.emit(body)
+
+func reset_checkpoints(body: Node2D) -> bool:
+	return seen.erase(body)
