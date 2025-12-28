2347d16b29cfbc9a721b1a9c4dea290741ed4b8b
Alex Comai <comai.alex@gmail.com>
2025-12-28 13:41:37 -0800

    zoom to speed

--- car/car.gd
+++ car/car.gd
@@ -4,6 +4,10 @@ class_name Car
 
 const INITIAL_SPEED: float = 50.0
 const MIN_GRIP: float = 0.05
+const WHEEL_SPIN_SCALE: float = 0.05
+
+
+@export var show_steer := true
 
 
 var wheel_angle: float = 0.0
@@ -20,10 +24,25 @@ var spin_momentum: float = 1.0:
 	set(value):
 		spin_momentum = clampf(value, 1.0, 2.0)
 var speed: float = INITIAL_SPEED
+var wheels: Array[AnimatedSprite2D]
+var smoke_timer: float = 0.0
 
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var label_grip: Label = $LabelGrip
+@onready var wheel_fl: AnimatedSprite2D = $WheelFL
+@onready var wheel_fr: AnimatedSprite2D = $WheelFR
+@onready var wheel_bl: AnimatedSprite2D = $WheelBL
+@onready var wheel_br: AnimatedSprite2D = $WheelBR
+@onready var smoke_left: GPUParticles2D = $SmokeLeft
+@onready var smoke_right: GPUParticles2D = $SmokeRight
+
+
+func _ready() -> void:
+	wheels = [wheel_fl, wheel_fr, wheel_bl, wheel_br]
+	for wheel in wheels:
+		wheel.play()
+		wheel.speed_scale = 0.0
 
 
 func _physics_process(delta: float) -> void:
@@ -31,12 +50,19 @@ func _physics_process(delta: float) -> void:
 
 	wheel_angle = lerp_angle(wheel_angle, turn * PI/4.0, 0.1)
 
+	if show_steer:
+		wheel_fl.rotation = lerp_angle(wheel_fl.rotation, wheel_angle, 0.1)
+		wheel_fr.rotation = lerp_angle(wheel_fr.rotation, wheel_angle, 0.1)
+
 	grip = remap(velocity.normalized().dot(-global_transform.y),
 				-1.0,
 				1.0,
 				MIN_GRIP,
 				1.0)
 	grip = pow(grip, 6.0)
+	var tire_smoke: bool = grip < 0.65
+	smoke_left.emitting = tire_smoke
+	smoke_right.emitting = tire_smoke
 	label_grip.text = "GRIP: %s" % snappedf(grip, 0.01)
 	label_grip.rotation = -rotation
 
@@ -44,13 +70,10 @@ func _physics_process(delta: float) -> void:
 	var small_speed: float = clampf(real_vel.length(), 0.0, 1.0)
 
 	var thrust_dir: Vector2 = -global_transform.y
-
 	velocity += thrust_dir * speed * delta * grip
-
 	var ideal_vel: Vector2 = -global_transform.y * velocity.length()
 
 	velocity = velocity.slerp(ideal_vel, 0.08 * grip)
-
 	velocity *= drag * remap(grip, MIN_GRIP, 1.0, 0.99, 1.0)
 
 	var heading = velocity.rotated(wheel_angle * small_speed * turn_grip)
@@ -58,18 +81,18 @@ func _physics_process(delta: float) -> void:
 	var ang_diff: float = angle_difference(car_angle, heading.angle() + PI/2.0)
 
 	spin_momentum += absf(ang_diff) * 0.8 * small_speed
-
 	spin = lerp(spin, ang_diff, 0.02 * grip)
-
 	var steer_strength: float = pow(speed, 0.95) * (3.0 / INITIAL_SPEED)
 
 	car_angle += spin * delta * steer_strength * spin_momentum * grip
-
 	rotation = car_angle
 
 	move_and_slide()
 
 	spin_momentum *= 0.99
+
+	for wheel in wheels:
+		wheel.speed_scale = (real_vel.length() + randfn(0.0, 0.1)) * WHEEL_SPIN_SCALE
 
 	#queue_redraw()
 
--- car/car_camera.gd
+++ car/car_camera.gd
@@ -4,6 +4,14 @@ extends Camera2D
 @export var car: Car
 
 
+var target_zoom: float = 2.0
+
+
 func _process(delta: float) -> void:
 	if car:
 		global_position = car.global_position
+		target_zoom = lerp(target_zoom,
+						remap(-car.speed, -250.0, -car.INITIAL_SPEED, 1.0, 2.0),
+						0.1)
+		target_zoom = clampf(target_zoom, 1.0, 2.0)
+		zoom = Vector2(target_zoom, target_zoom)
