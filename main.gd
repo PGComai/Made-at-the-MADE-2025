fcb80089f4cd26fd71ace089b0e7f58ee8ec9071
Ava Pek <code@exodrifter.space>
2025-12-28 18:00:25 -0800

    Update life timer

--- main.gd
+++ main.gd
@@ -4,8 +4,11 @@ extends Control
 const GAME_OVER = preload("uid://cdmwdc4fob4s2")
 
 
+@export var car: Car
 @onready var label_time_left: Label = $LabelTimeLeft
 
+func _process(_delta: float) -> void:
+	label_time_left.text = "%.2f" % car.life
 
 func game_over() -> void:
 	var new_game_over = GAME_OVER.instantiate()
